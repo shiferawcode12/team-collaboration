6e3468ef72e8b331a4afeea74cdfeafa63c83eda
Ephremki <129614801+Ephremki@users.noreply.github.com>
2023-05-19 14:16:38 +0300

    Add files via upload

---
+++ game/game.js
@@ -0,0 +1,24 @@
+
+const choices = ['rock', 'paper', 'scissors'];
+const resultDiv = document.getElementById('result');
+
+function playGame(userChoice) {
+    // Generate computer choice
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    // Determine winner
+    if (userChoice === computerChoice) {
+        resultDiv.textContent = "It's a tie!";
+        resultDiv.style.color = "#333";
+    } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        resultDiv.textContent = "You win!";
+        resultDiv.style.color = "#4CAF50";
+    } else {
+        resultDiv.textContent = "You lose!";
+        resultDiv.style.color = "#f44336";
+    }
+}

255aa69f1d8854599fa7784b419f8d384a2fbb0e
Ephremki <129614801+Ephremki@users.noreply.github.com>
2023-05-19 15:19:53 +0300

    delete comment

--- game/game.js
+++ game/game.js
@@ -3,10 +3,7 @@ const choices = ['rock', 'paper', 'scissors'];
 const resultDiv = document.getElementById('result');
 
 function playGame(userChoice) {
-    // Generate computer choice
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-
-    // Determine winner
     if (userChoice === computerChoice) {
         resultDiv.textContent = "It's a tie!";
         resultDiv.style.color = "#333";
